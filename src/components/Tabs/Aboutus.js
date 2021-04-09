078122bae59ba6b0ba79a9a92992c21591e930b5
sama16 <samaalmallak7@gmail.com>
2021-04-09 03:07:59 +0300

    first commit

--- src/components/Tabs/Aboutus.js
+++ src/components/Tabs/Aboutus.js
@@ -1,12 +1,13 @@
 import React from 'react';
-
+import Images from "./images";
 import img from '../Images/sample.png';
 import Hala from '../Images/Hala.JPG';
 
 
 export default function Aboutus() {
     return (
-      <div style = {{marginBottom : "150px" ,
+    <div>
+        <div style = {{marginBottom : "150px" ,
       marginTop:" 70px " , textAlign: 'center', justifyContent: 'center' ,
        alignItems: 'center' , 
         }}>
@@ -14,11 +15,15 @@ export default function Aboutus() {
         borderBottom : "4px solid red" ,fontSize : '40px', marginBottom : '30px'
         }}>About us </h2>
         <p style = {{fontSize : '20px'}}>Creativity and fun are our ingredients for awesome work.</p>
+      
     
 
 
     </div>
-        
+      <div style={{display: "flex"}}>
+      <Images/>
+      </div>
+        </div>
     )
 }
 
