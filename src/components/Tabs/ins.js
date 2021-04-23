f0f2ec7d1e8728c71d394f5d346543e372d644bd
Tala55 <talamohann@gmail.com>
2021-04-23 23:50:49 +0300

    first commit

--- src/components/Tabs/ins.js
+++ src/components/Tabs/ins.js
@@ -1,17 +1,19 @@
 import React from 'react'
 import './ins.scss'
 import {AiFillFire} from 'react-icons/ai'
-import  Inspiration from './inspiration'
+import Tips from '../Images/tips.png'
+
 
 function ins() {
     
     return (
         <div>
 				<div>
-					<Inspiration/>
 					</div>
     <div>
-		<h2 style={{marginBottom:"50px",marginTop:"100px",textAlign:"center",fontFamily:"cursive",fontSize:"40px",color:"black"}}><AiFillFire style={{color:'black',marginTop:'-11px'}}/>Some successful stories that created history<AiFillFire style={{color:'black',marginTop:'-5px'}}/> </h2>
+		<h2 style={{marginBottom:"50px",marginTop:"100px",textAlign:"center",fontFamily:"cursive",fontSize:"40px",color:"black",fontWeight:"bolder"}}>Some successful stories that created history</h2>
+		<p style={{fontFamily:"cursive",fontSize:"20px",textAlign:'center'}}>Success to me means Believing in yourself, loving yourself and being happy with who you are. The basic tenet of those who wish to succeed is to be willing to fail. You must take 100% responsibility for EVERYTHING you experience in life. We learn from our failures. A successful person accepts personal responsibility and takes the creator role in their lives. No one can change our lives but ourselves. We are the master of our own universe!!
+Everything we want in life involves risks.</p>
 	</div>
 
 <nav class="nav">
@@ -53,7 +55,58 @@ function ins() {
 		</li>
 	</ul>
 </nav>
-				
+		<div style={{backgroundColor:"#f1f7ff",  marginRight: '-150px',
+    marginLeft:' -130px',
+    padding: '100px 40px',
+	marginBottom:"100px",
+	marginTop:'79px'
+}}>
+			
+		<p style={{fontFamily:"cursive",textAlign:"center",fontSize:"30px"}}>Time management is the process of planning and controlling how much time to spend on specific activities. Good time management enables an individual to complete more in a shorter period of time, lowers stress, and leads to career success. </p>	
+		<img style={{display: 'block',
+			marginLeft: 'auto',
+			marginRight: 'auto',
+			width: '50%', }}src={Tips}/> 
+		</div>		
+		<div>
+			<h2 style={{textAlign:"center",fontSize:"50px",}}>
+			Benefits of Time Management
+			</h2>
+			<p style={{marginTop:"60px",fontSize:"20px"}}>
+				 
+<span style={{color:'#a80c2b',fontWeight:'bold',}}>
+1. Stress relief</span>
+<br/>
+Making and following a task schedule reduces anxiety. As you check off items on your “to-do” list, you can see that you are making tangible progress. This helps you avoid feeling stressed out with worry about whether you’re getting things done.
+
+ <br/>
+ </p>
+ <p style={{fontSize:"20px"}}>
+ <span style={{color:'#a80c2b',fontWeight:'bold',}}>
+2. More time</span>
+<br/>
+
+Good time management gives you extra time to spend in your daily life. People who can time-manage effectively enjoy having more time to spend on hobbies or other personal pursuits.
+
+<br/>
+</p>
+<p style={{fontSize:"20px"}}>
+<span style={{color:'#a80c2b',fontWeight:'bold',}}>
+3. More opportunities</span>
+<br/>
+Managing time well leads to more opportunities and less time wasted on trivial activities. Good time management skills are key qualities that employers look for. The ability to prioritize and schedule work is extremely desirable for any organization.
+
+<br/>
+</p>
+<p style={{fontSize:"20px"}}> 
+<span style={{color:'#a80c2b',fontWeight:'bold',}}>
+4. Ability to realize goals</span>
+<br/>
+Individuals who practice good time management are able to better achieve goals and objectives, and do so in a shorter length of time.
+
+<br/>
+			</p>
+		</div>
      </div>
     )
 }
